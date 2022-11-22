6df547de1ae30b94ac2e47c9c867b2100426c17e
giathai2605 <chiyeu1nguoi.2003@gmail.com>
2022-11-22 14:40:41 +0700

    Sua lai 1 ti Product

--- admin/products/list.php
+++ admin/products/list.php
@@ -1,5 +1,4 @@
 
-=======
 <article class="w-[85%]">
     <header style="border-radius: 10px; background: #fff; box-shadow: 35px 35px 70px #bebebe,
 -35px -35px 70px #ffffff;
@@ -179,5 +178,4 @@
 </div>
 </body>
 
->>>>>>> thaidolphin
-</html>
+
