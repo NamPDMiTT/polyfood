d909ff8a0c145385c37eec4a88ac52ec256354f6
lecongtienl <congtiendev@gmail.com>
2022-11-29 21:57:30 +0700

    fix lỗi ảnh

--- site/page/home.php
+++ site/page/home.php
@@ -100,33 +100,33 @@
                 <?php extract($item); ?>
                 <div class="product__item w-full sm:w-1/3 lg:w-1/4 p-4">
                     <div class="rounded-xl">
-                      <a href="<?= $SITE_URL ?>/page/detail.php?product_id=<?= $product_id ?>">
-                      <div class="product__img">
-                            <img class="w-full object-cover object-center rounded-t-xl" src="<?= $CONTENT_URL ?>/images/products/<?= $image ?>" alt="content" />
-                        </div>
-                      </a>
-                        <div class="product__info flex flex-col gap-1 rounded-b-xl w-full p-3 border border-gray-300">
                         <a href="<?= $SITE_URL ?>/page/detail.php?product_id=<?= $product_id ?>">
-                        <h2 class="text-gray-900 font-medium title-font"><?= $product_name ?></h2>
-                            <p class="leading-relaxed text-xs limited__content-2">
-                                <?= $detail ?>
-                            </p>
-                            <div class="product__cart-rate flex justify-between items-center my-1">
-                                <span class="text-sm text-[#FAB500]">★★★★★</span>
-                                <span class="text-sm text-orange-600 font-medium"><?= number_format($price, 0, '', '.') ?>đ</span>
+                            <div class="product__img">
+                                <img class="w-full min-w-[200px] h-[150px] lg:h-[200px] object-cover object-center rounded-t-xl" src="<?= $CONTENT_URL ?>/images/products/<?= $image ?>" alt="content" />
                             </div>
-            </a>
+                        </a>
+                        <div class="product__info flex flex-col gap-1 rounded-b-xl w-full p-3 border border-gray-300">
+                            <a href="<?= $SITE_URL ?>/page/detail.php?product_id=<?= $product_id ?>">
+                                <h2 class="text-gray-900 font-medium title-font"><?= $product_name ?></h2>
+                                <p class="leading-relaxed text-xs limited__content-2">
+                                    <?= $detail ?>
+                                </p>
+                                <div class="product__cart-rate flex justify-between items-center my-1">
+                                    <span class="text-sm text-[#FAB500]">★★★★★</span>
+                                    <span class="text-sm text-orange-600 font-medium"><?= number_format($price, 0, '', '.') ?>đ</span>
+                                </div>
+                            </a>
                             <form action="<?= $SITE_URL ?>/cart/index.php?btn_order" method="post">
-                            <input type="hidden" value="<?= $product_id ?>" id="product_id" name="product_id">
-                            <input type="hidden" id="image" name="image" value="<?=$image?>">
-                            <button class="add-to-cart flex gap-2 sm:gap-3 items-center justify-center w-full px-4 py-2 text-xs font-medium text-white bg-orange-600 border border-transparent rounded-md hover:bg-indigo-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-orange-600">
-                            
-                            <span> Thêm vào giỏ </span>
-                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                               </svg>
-                           
-                           </button>
+                                <input type="hidden" value="<?= $product_id ?>" id="product_id" name="product_id">
+                                <input type="hidden" id="image" name="image" value="<?= $image ?>">
+                                <button class="add-to-cart flex gap-2 sm:gap-3 items-center justify-center w-full px-4 py-2 text-xs font-medium text-white bg-orange-600 border border-transparent rounded-md hover:bg-indigo-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-orange-600">
+
+                                    <span> Thêm vào giỏ </span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                                    </svg>
+
+                                </button>
                             </form>
                         </div>
                     </div>
