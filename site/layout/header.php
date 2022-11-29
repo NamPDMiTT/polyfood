e67bfe753971e3ac715f2d5fe0a214720888413b
DangPhuongNam7303 <phuongnam7303@gmail.com>
2022-11-29 15:21:29 +0700

    update

--- site/layout/header.php
+++ site/layout/header.php
@@ -15,6 +15,6 @@
 <body class="font-montserrat">
     <header class="hidden lg:block text-gray-700 bg-white border-b border-gray-200 sticky top-0 z-50">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <a class="flex font title-font font-medium items-center mb-4 md:mb-0" href="#" target="_blank">
+            <a class="flex font title-font font-medium items-center mb-4 md:mb-0" href="<?=$SITE_URL?>/page/index.php" target="_blank">
                 <span class="text-orange-600">poly</span><span class="text-blue-600 font-semibold">F</span><span class="text-orange-600 font-semibold">oo</span><span class="text-green-600 font-semibold">d</span>
             </a>
