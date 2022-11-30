b4c233dbdebbb8941db6695857385db37ae67714
giathai2605 <chiyeu1nguoi.2003@gmail.com>
2022-11-30 23:37:47 +0700

    update

--- site/page/product.php
+++ site/page/product.php
@@ -45,7 +45,7 @@ if (exist_param("category_id")) {
                     <?php } ?>
                     <li><a href="<?= $SITE_URL ?>/page/introduce.php">Giới thiệu</a></li>
                     <li><a href="<?= $SITE_URL ?>/page/product.php">Sản Phẩm</a></li>
-                    <li><a href="<?= $SITE_URL ?>/page/blog.php">Blog</a></li>
+                    <li><a href="<?= $SITE_URL ?>/post/index.php">Blog</a></li>
                     <li>
                         <div class="relative inline-block text-left">
                             <div>
@@ -513,7 +513,7 @@ Quên mật khẩu</button>";
                 </a>
             </li>
             <li>
-                <a href="<?= $SITE_URL ?>/page/blog.php">
+                <a href="<?= $SITE_URL ?>/post/index.php">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 sm:w-10">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
                     </svg>
