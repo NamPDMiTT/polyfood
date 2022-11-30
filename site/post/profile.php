26241a12035b2d34d24bf6af179aab30b17128fa
giathai2605 <chiyeu1nguoi.2003@gmail.com>
2022-12-01 02:48:55 +0700

    Thai

--- site/post/profile.php
+++ site/post/profile.php
@@ -2,7 +2,7 @@
       <aside class="">
           <div style="background-image: url('../IMG/anh-bia.jpg')" class="shadow rounded-lg">
               <div class="flex flex-col gap-1 text-center items-center backdrop-blur-sm p-16">
-                  <img class="h-32 w-32 bg-white p-2 rounded-full shadow mb-4" src="../../content/images/posts/tien.jpg" alt="" />
+                  <img class="h-32 w-32 bg-white p-2 rounded-full shadow mb-4" src="<?= $CONTENT_URL ?>/images/users/<?= $_SESSION['user']['image'] ?>" alt="" />
                   <p class="font-semibold text-white">Lê Công Tiến</p>
                   <p class="text-white text-xs">congtiendev</p>
                   <div class="text-sm leading-normal text-gray-100 flex justify-center items-center">
