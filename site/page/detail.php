0d5884b49e70d00eaf7c5368a0d7cd28e2b18630
NamPDMiTT <102017513+NamPDMiTT@users.noreply.github.com>
2022-12-18 12:52:10 +0700

    fix css, link

--- site/page/detail.php
+++ site/page/detail.php
@@ -232,7 +232,7 @@ Quên mật khẩu</button>";
 
                 <div class="grid grid-cols-[48px,auto] gap-8">
                     <div class="rounded-full rounded-2 rounded-red-500 w-[48px] h-[48px]">
-                        <img src="<?= $CONTENT_URL ?>/images/users/<?= $fb['img_user'] ?>" alt="">
+                        <img src="<?= $CONTENT_URL ?>/images/users/<?= $fb['img_user'] ?>" style="border-radius:50%" alt="">
                     </div>
                     <div class="">
                         <div class="flex items-center space-x-4">
@@ -456,7 +456,7 @@ Quên mật khẩu</button>";
             </div>
         </div>
     </footer>
-    <a style="box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37)" id="cart" href="<?= $SITE_URL ?>/cart/all-my-cart.php" class="fixed right-0 z-50 flex items-center justify-center w-16 h-16 mb-8 mr-8 text-gray-800 bg-orange-600 border border-gray-300 rounded-full shadow-sm sm:w-20 sm:h-20 animate-bounce bottom-8" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white w-9 h-9">
+    <a style="box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37)" id="cart" href="<?= $SITE_URL ?>/cart/index.php?my-cart.php" class="fixed right-0 z-50 flex items-center justify-center w-16 h-16 mb-8 mr-8 text-gray-800 bg-orange-600 border border-gray-300 rounded-full shadow-sm sm:w-20 sm:h-20 animate-bounce bottom-8" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white w-9 h-9">
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
         </svg>
     </a>
--- staff/page/menu.php
+++ staff/page/menu.php
@@ -27,7 +27,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                         </svg>
-                        <a href="<?= $SITE_URL ?>/page/index.php" class="font-medium text-sm text-gray-500">Home</a>
+                        <a href="<?= $STAFF_URL ?>/page/index.php" class="font-medium text-sm text-gray-500">Home</a>
                     </li>
 
                     <li class="menu__bar-item flex  gap-3 items-center">
@@ -70,6 +70,17 @@
                         </svg>
                         <a href="<?= $STAFF_URL ?>/categories/index.php?btn_list"
                             class="font-medium text-sm text-gray-500">Category</a>
+                    </li>
+                    
+                    <li class="menu__bar-item flex  gap-3 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6 text-gray-500">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+                        </svg>
+
+                        <a href="<?= $SITE_URL ?>/page/index.php" class="font-medium text-sm text-gray-500">Về trang
+                            chủ</a>
                     </li>
                 </ul>
             </aside>
